33d77c980cdb24e52a3fcf2723bce4c2b1e8efea
Victor Alves <victor.alvesf7@gmail.com>
2018-09-15 10:37:18 -0400

    Multiplicação funcional JS

---
+++ multiply.js
@@ -0,0 +1,17 @@
+const {add, inverse, subtract} = require('./add.subtract-refactoring');
+
+const multiply = (x) => (y) => {
+    let result = 0;
+    const addX = add( y ); //9
+    const decrement1 = subtract( x ); // x = 5
+    let cont = 0;
+
+    while(decrement1( cont ) != 0) {
+        result = addX(result); // 9 + 9 (5x)
+        cont++;
+    }
+
+    return result;
+}
+
+console.log(' 5 x 9 = ', multiply(5)(9));
