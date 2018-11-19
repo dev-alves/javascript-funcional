0787df1e086bd07f158ce1ccdddd8345b684b676
Victor Alves <victor.alvesf7@gmail.com>
2018-11-19 10:35:48 -0400

    Funcional concluído

--- arrays.js
+++ arrays.js
@@ -5,24 +5,24 @@ const somenteDois = () => 2;
 console.log('Criando arrays >>> ', criandoArray( 5, somenteDois ));
 
 //recriando função
-const criandoArrayCom = ( criacao ) => ( length ) => 
+const REFATORANDO_criandoArray = ( criacao ) => ( length ) => 
     Array.from( { length }, criacao );
 
-console.log('criandoArray2 >>> ', criandoArrayCom( somenteDois)(7) );
+console.log('criandoArray2 >>> ', REFATORANDO_criandoArray( somenteDois)(7) );
 
 const tamanhoCinco = () => 5;
 const somentePares = ( v, i ) => ( i === 0 ) ? i + 2 : i + ( i + 2 );
 const somenteImpares = ( v, i ) => ( i ===0 ) ? i + 1 : i + ( i + 1 );
 
-const arraySomenteComCincoPares = criandoArrayCom( somentePares );
-const arraySomenteComCincoImpares = criandoArrayCom ( somenteImpares );
+const arraySomenteComCincoPares = REFATORANDO_criandoArray( somentePares );
+const arraySomenteComCincoImpares = REFATORANDO_criandoArray ( somenteImpares );
 
 console.log('arraySomenteComCincoPares >>> ', arraySomenteComCincoPares(tamanhoCinco()));
 console.log('arraySomenteComCincoImpares >>> ', arraySomenteComCincoImpares(tamanhoCinco()));
 
 //refatoração
 const criandoArrayPatternCom = ( criacao ) => ({
-    com: criandoArrayCom( criacao )
+    com: REFATORANDO_criandoArray( criacao )
 })
 
 console.log('criandoArrayPatternCom >>> ', criandoArrayPatternCom(somentePares).com(tamanhoCinco()));
@@ -30,7 +30,7 @@ console.log('criandoArrayPatternCom >>> ', criandoArrayPatternCom(somentePares).
 const criandoArrayComEObter = ( fn ) => {
     return {
         comTamanho: ( tam ) => {
-            result = criandoArrayCom ( fn ) ( tam );
+            result = REFATORANDO_criandoArray ( fn ) ( tam );
 
             return {
                 get: ( ( result ) => ( ...pos ) =>
@@ -44,6 +44,40 @@ const criandoArrayComEObter = ( fn ) => {
 }
 
 console.log('criandoArrayComEObter >>> ', criandoArrayComEObter( somentePares ).comTamanho( 10 ).get( 0, 3 ));
+
+//refatorando
+const _get = ( arr ) => ( ...pos ) =>
+    ( pos.length === 1 )
+    ? arr [ pos ]
+    : arr.slice( ...pos )
+
+const REFATORADO_criandoArrayComEObter = ( fn ) => {
+    return {
+        comTamanho: ( tam ) => ({
+            get: _get ( REFATORANDO_criandoArray ( fn ) ( tam ) )
+        })
+    }
+}
+
+
+console.log('REFATORADO_criandoArrayComEObter >>> ', REFATORADO_criandoArrayComEObter( somentePares )
+    .comTamanho( 10 )
+    .get( 0, 3 ));
+
+
+//refatorando ainda mais
+const _comTamanho = ( fn ) => ( tam ) => ({
+    get: _get ( REFATORANDO_criandoArray( fn ) ( tam ) )
+}) 
+
+const REFATORADO_criandoArrayComGet = ( fn ) => ({
+    comTamanho: _comTamanho ( fn )
+})
+
+
+console.log('REFATORADO_criandoArrayComGet >>> ', REFATORADO_criandoArrayComGet( somenteImpares )
+    .comTamanho( 5 )
+    .get( 0, 3 ));
 
 
 const arraysMultiplicacao = [ 2, 4, 5, 7, 9 ];
@@ -62,9 +96,51 @@ const operador = ( mutiplicador ) => {
         }
     }
 }
-
-
 console.log( 'operador >>> ', operador( multiplicacao ).inserirArray( arraysMultiplicacao ).intervalo( 0, 3 ) );
+
+
+//refatorando
+const _intervalo = ( mutiplicador ) => ( arr ) => ( ...pos ) => 
+    ( pos.length === 1) 
+    ? ( arr[ pos ]*mutiplicador )
+    : arr.slice( ...pos ).map( element => element * mutiplicador );
+
+
+const _inserirArray = ( mutiplicador ) => ( arr ) => ({
+    intervalo: _intervalo (mutiplicador) ( arr )
+}) 
+const REFATORANDO_operador = ( mutiplicador ) => ({
+    inserirArray: _inserirArray( mutiplicador )
+})
+
+console.log('REFATORANDO_operador >>> ', REFATORANDO_operador( 5 ).inserirArray( arraysMultiplicacao ).intervalo( 0, 3 ) );
+
+
+//FUNCIONALLL :DDD
+const functionalCriandoArray = ( criacao ) => ( length ) => Array.from( { length }, criacao );
+const functionalCriandoArrayCom = ( fn ) => functionalCriandoArray( fn )
+const funcionalComTamanho = ( tam, criacao ) => criacao( tam )
+
+const pegar = ( pos, arr ) =>
+    ( pos.map )
+    ? arr.slice(...pos)
+    : arr[ pos ]
+
+console.log('funcional  >>>> ', funcionalComTamanho( 10, functionalCriandoArrayCom( somentePares ) ) );
+console.log('funcional refatorado  >>>> ', pegar( [ 1, 4 ], funcionalComTamanho( 10, functionalCriandoArrayCom( somentePares ) ) ) );
+
+
+//FUNCIONAL P/2 :D
+const funcional_intervalo = ( pos, arr ) =>
+    ( pos.map )
+    ? arr.slice(...pos)
+    : arr[ pos ]
+
+const funcional_operador = ( multi, arr ) => arr.slice().map( element => element * multi ); 
+
+console.log( 'funcional refatorado 2x >>> ', funcional_intervalo( [ 0, 3 ], funcional_operador( 5,  [ 1, 2, 3, 5, 6] )  ) );
+
+
 
 
 //includes()
